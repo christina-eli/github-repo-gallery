1c1d2453bec84acc81e5e964d75c6f77237bde8a
Christina Hawkins <christinaehawkins@gmail.com>
2023-11-12 23:01:08 -0500

    Fetch Repo Data

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 //create global variables
 const overview = document.querySelector (".overview"); //profile info div
 const username = "christina-eli";
+const repoList= document.querySelector(".repo-list"); 
 
 //fetch API JSON Data
 const gitUserInfo = async function () {
@@ -29,5 +30,23 @@ const displayUserInfo = function(data) {
         </div> 
         `;
 
-overview.append(div);
+    overview.append(div);
+    gitRepos();
 };
+
+//fetch repos
+const gitRepos = async function () {
+    const fetchRepos = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const repoData = await fetchRepos.json();
+    displayRepos(repoData);
+  };
+ 
+//display repos
+  const displayRepos = function (repos) {
+    for (const repo of repos) {
+      const repoItem = document.createElement("li");
+      repoItem.classList.add("repo");
+      repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+      repoList.append(repoItem);
+    }
+  };
