4bfd61c57e3d3e60a3190b7134e45cd7943acf98
Christina Hawkins <christinaehawkins@gmail.com>
2023-11-12 22:44:36 -0500

    initial commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+//create global variables
+const overview = document.querySelector (".overview"); //profile info div
+const username = "christina-eli";
+
+//fetch API JSON Data
+const getUserInfo = async function () {
+    const userInfo = await fetch ('https://api.github.com/users/${username}');
+    const data = await userInfo.json();
+    displayUserInfo(data);
+};
+
+//call function at the end
+gitUserInfo();
+
+//Fetch & Display User Information
+const displayUserInfo = function(data) {
+    const div = document.createElement("div");
+    div.classList.add("user-info");
+    div.innerHTML = `
+        <figure>
+            <img alt="user avatar" src=${data.avatar_url} />
+         </figure>
+        <div>
+            <p><strong>Name:</strong> ${data.name}</p>
+            <p><strong>Bio:</strong> ${data.bio}</p>
+            <p><strong>Location:</strong> ${data.location}</p>
+            <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
+        </div> 
+        `;
+
+overview.append(div);
+};
